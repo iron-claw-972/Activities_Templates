335838ad6b9860e3667d4dc50bb18250336b1301
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2021-09-20 16:01:02 -0700

    Update RobotContainer.java

--- Activity_1/src/main/java/frc/robot/RobotContainer.java
+++ Activity_1/src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.*;
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * (including subsystems, comm+ands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   //subsystems
