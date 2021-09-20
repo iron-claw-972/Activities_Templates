c66b90164dc0216c952801306cf106228275c25b
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2021-09-20 14:35:51 -0700

    Update Constants.java

--- Activity_1/src/main/java/frc/robot/Constants.java
+++ Activity_1/src/main/java/frc/robot/Constants.java
@@ -19,15 +19,15 @@ public final class Constants {
   public static final class DriveConstants {
     //TODO: 1. Set this to the motor ports
     public static final int kLeftMotor1Port = 1;
-    public static final int kLeftMotor2Port = 1;
-    public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 2;
+    public static final int kLeftMotor2Port = 2;
+    public static final int kRightMotor1Port = 4;
+    public static final int kRightMotor2Port = 5;
 
     //make sure your controller is 0
     public static final int kControllerPort = 0;
 
     //TODO: 2. Change the joystick axis for arcade drive
-    public static final int kLeftJoyAxis = -1;
-    public static final int kRightJoyAxis = -1;
+    public static final int kLeftJoyAxis = 1;
+    public static final int kRightJoyAxis = 5;
   }
 }
