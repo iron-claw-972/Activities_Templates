9bf8116103017c8c6c5541f62d7724ce3a628cda
Vickles <agninja88@gmail.com>
2021-09-24 14:51:02 -0700

    uwu

--- Activity_1/src/main/java/frc/robot/Constants.java
+++ Activity_1/src/main/java/frc/robot/Constants.java
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/
+  /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
@@ -15,19 +15,19 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class Constants { 
   public static final class DriveConstants {
     //TODO: 1. Set this to the motor ports
-    public static final int kLeftMotor1Port = 1;
-    public static final int kLeftMotor2Port = 1;
-    public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 2;
+    public static final int kLeftMotor1Port = 15;
+    public static final int kLeftMotor2Port = 14;
+    public static final int kRightMotor1Port = 1;
+    public static final int kRightMotor2Port = 18;
 
     //make sure your controller is 0
     public static final int kControllerPort = 0;
 
     //TODO: 2. Change the joystick axis for arcade drive
-    public static final int kLeftJoyAxis = -1;
-    public static final int kRightJoyAxis = -1;
+    public static final int kLeftJoyAxis = 1;
+    public static final int kRightJoyAxis = 4;
   }
 }
