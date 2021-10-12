ac6ee9412db272c4eda4ba3c19be5672009b80ab
Andrea Duan <90481687+andrduan@users.noreply.github.com>
2021-10-12 14:35:34 -0700

    Arm Subsystem

---
+++ Activity_1/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants.DriveConstants;
+
+public class ArmSubsystem extends SubsystemBase {
+
+  TalonSRX leftArmMotor = new TalonSRX(DriveConstants.kLeftArmMotorPort);
+  TalonSRX rightArmMotor = new TalonSRX(DriveConstants.kRightArmMotorPort);
+  TalonSRX leftIntakeMotor = new TalonSRX(DriveConstants.kleftIntakeMotorPort);
+  TalonSRX rightIntakeMotor = new TalonSRX(DriveConstants.krightIntakeMotorPort);
+
+  public ArmSubsystem() {
+    rightArmMotor.set(ControlMode.Follower, DriveConstants.kleftArmMotorPort);       
+    rightIntakeMotor.set(ControlMode.Follower, DriveConstants.kleftIntakeMotorPort);
+  }
+ 
+  public void run(double armPower) {
+    leftArmMotor.set(ControlMode.PercentOutput, armPower);
+    leftIntakeMotor.set(ControlMode.PercentOutput, pow);
+  }
+}
