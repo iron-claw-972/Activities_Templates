586ab3faa9b26ced4cc0687960f8a8219a80544a
nicshima <90671126+nicshima@users.noreply.github.com>
2021-10-26 16:04:27 -0700

    PID

--- Activity_1/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Activity_1/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,38 +2,39 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.revrobotics.CANSparkMax;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.controller.PIDController;
 
 import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
-    TalonSRX leftArmMotor1 = new TalonSRX(ArmConstants.kLeftArmMotorPort);
-    TalonSRX rightArmMotor1 = new TalonSRX(ArmConstants.kRightArmMotorPort);
-    TalonSRX leftIntakeMotor2 = new TalonSRX(ArmConstants.kLeftIntakeMotorPort);
-    TalonSRX rightIntakeMotor2 = new TalonSRX(ArmConstants.kRightIntakeMotorPort);
-    PIDController pid = new PIDController(1, 0, 0);
+    TalonSRX leftArmMotor = new TalonSRX(ArmConstants.kLeftArmMotorPort);
+    TalonSRX rightArmMotor = new TalonSRX(ArmConstants.kRightArmMotorPort);
+    TalonSRX leftIntakeMotor = new TalonSRX(ArmConstants.kLeftIntakeMotorPort);
+    TalonSRX rightIntakeMotor = new TalonSRX(ArmConstants.kRightIntakeMotorPort);
+    PIDController pid = new PIDController(0.001, 0, 0);
 
     public ArmSubsystem() {
         leftArmMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 100);
 
-        rightIntakeMotor2.set(ControlMode.Follower, ArmConstants.kLeftIntakeMotorPort);
-        rightArmMotor1.set(ControlMode.Follower, ArmConstants.kLeftArmMotorPort);
+        rightIntakeMotor.set(ControlMode.Follower, ArmConstants.kLeftIntakeMotorPort);
+        rightArmMotor.set(ControlMode.Follower, ArmConstants.kLeftArmMotorPort);
     }
 
     public void run(double armPower) {
-        leftArmMotor1.set(ControlMode.PercentOutput, armPower);
-        leftIntakeMotor2.set(ControlMode.PercentOutput, armPower);
+        leftArmMotor.set(ControlMode.PercentOutput, armPower);
+        leftIntakeMotor.set(ControlMode.PercentOutput, armPower);
     }
 
     public void runPID(double setpoint) {
         double currentPosition = leftArmMotor.getSensorCollection().getQuadraturePosition();
-        leftArmMotor1.set(ControlMode.PercentOutput, pid.calculate(currentPosition, setpoint));
-        leftIntakeMotor2.set(ControlMode.PercentOutput, 10);
+        leftArmMotor.set(ControlMode.PercentOutput, pid.calculate(currentPosition, setpoint));
+        leftIntakeMotor.set(ControlMode.PercentOutput, 10);
     }
 }
